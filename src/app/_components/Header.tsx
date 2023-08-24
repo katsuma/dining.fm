5f4294b90476f677e846c3be03d0ccb73ed8627e
Ryo Katsuma <katsuma@gmail.com>
2023-08-24 14:01:01 +0900

    Refine layout

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import styles from './Header.module.css';
 
 export default function Header() {
-  const logoPath = './logo.svg';
+  const logoPath = '/logo.svg';
 
   return (
     <header className={styles.header}>
       <h1 className={styles.header__logo}>
-        <Image src={logoPath} alt={'dining.fm'} width={192} height={62} priority={true} />
+        <Link href={'/'}>
+          <Image src={logoPath} alt={'dining.fm'} width={192} height={62} priority={true} />
+        </Link>
       </h1>
     </header>
   );
