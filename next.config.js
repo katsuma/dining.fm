89590c93afc1f40a31df2001a8da081ddb692570
ryo katsuma <katsuma@gmail.com>
2023-09-23 12:46:41 +0900

    Revert "Redirect www to non-www host url"

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-        {
-            source: "/:path*",
-            has: [{ type: "host", value: "www.dining.fm" }],
-            destination: "https://dining.fm/:path*",
-            permanent: true,
-        },
-    ];
-  },
   images: {
     remotePatterns: [
       {
