83ed628f82f1ddb25ffeb94e8572135756a9b673
Ryo Katsuma <katsuma@gmail.com>
2024-01-08 18:15:42 +0900

    Sort podcast latform link by popularity

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -7,12 +7,12 @@ export default function Footer() {
         <div className={styles.footer_content}>
           <p className={styles.caption}>購読</p>
           <ul className={styles.list_links}>
-            <li><a href="https://podcasts.apple.com/us/podcast/dining-fm/id1668849655">Apple Podcasts</a></li>
             <li><a href="https://open.spotify.com/show/3wSB2J20uqON5nPhCmMia5">Spotify</a></li>
+            <li><a href="https://podcasts.apple.com/us/podcast/dining-fm/id1668849655">Apple Podcasts</a></li>
+            <li><a href="https://listen.style/p/diningfm">LISTEN</a></li>
+            <li><a href="https://overcast.fm/itunes1668849655/dining-fm">Overcast</a></li>
             <li><a href="https://music.amazon.co.jp/podcasts/2a16e7f2-2c99-4d85-8ee7-0916a6c1f56d/dining-fm">Amazon Music</a></li>
             <li><a href="https://podcasts.google.com/feed/aHR0cHM6Ly9hbmNob3IuZm0vcy9kODk3OTBmNC9wb2RjYXN0L3Jzcw">Google Podcasts</a></li>
-            <li><a href="https://overcast.fm/itunes1668849655/dining-fm">Overcast</a></li>
-            <li><a href="https://listen.style/p/diningfm">LISTEN</a></li>
           </ul>
         </div>
 
