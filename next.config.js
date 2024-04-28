83ed316337bdc2e026bc4644aa4e122a4c55910d
Ryo Katsuma <katsuma@gmail.com>
2024-04-28 10:45:00 +0900

    Adds cache settings in header

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@ const withMDX = require('@next/mdx')()
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
